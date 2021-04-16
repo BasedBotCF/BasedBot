196ed4863d752af135b25dcecd18eee2658f34f6
Kaz <58613384+KazUpTV@users.noreply.github.com>
2021-04-16 14:35:37 -0400

    nevermind doesn't work with my needs

--- app.js
+++ app.js
@@ -36,20 +36,21 @@ const bot = mineflayer.createBot({
     session
 })
 
-//const activities_list = [
-  //"with the .help command.", 
-  //"with pornhub.com",
-  //"with some code", 
-  //"with NodeJS",
-  //"with ddos scripts"
-  //]; // creates an arraylist containing phrases you want your bot to switch through.
+const activities_list = [
+  "with the .help command.", 
+  "with pornhub.com",
+  "with some code", 
+  "with NodeJS",
+  "with ddos scripts",
+  "with Basedbot.cf"
+  ]; // creates an arraylist containing phrases you want your bot to switch through.
 
-  //client.on('ready', () => {
-    //setInterval(() => {
-        //const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
-        //client.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
-    //}, 10000); // Runs this every 10 seconds.
-//});
+  client.on('ready', () => {
+    setInterval(() => {
+        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
+        client.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
+    }, 10000); // Runs this every 10 seconds.
+  });
 
 
 bot.on('ready', async => {
@@ -319,10 +320,10 @@ bot.on('chat', (username, message) => {
 
   client.on("ready", async => {
     console.log("Discord Bot Online")
-    client.user.setActivity("on basedbot.cf", {
-      type: "STREAMING",
-      url: "basedbot.cf"
-    });
+    //client.user.setActivity("on basedbot.cf", {
+      //type: "STREAMING",
+      //url: "basedbot.cf"
+    //});
   });
   
   bot.on("login", async => {
